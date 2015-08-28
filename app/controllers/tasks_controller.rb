8ff270adafd18995b8c49e9a48ba7998934f95f5
Ashleigh <ashleighalmeida@gmail.com>
2015-08-28 11:57:11 -0400

    datepicker + task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,14 @@ class TasksController < ApplicationController
   @task = Task.new
 end
 
+def create
+    @task = Task.create(task_params)
+    redirect_to root_path
+  end
+  
+  private
+  def task_params
+    params.require(:task).permit(:title, :note, :completed)
+  end
+
 end
